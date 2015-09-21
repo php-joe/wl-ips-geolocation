1599ff57035abb276a7e903f37eeb29805433b1a
php-joe <joseph@schnauzerdesign.com>
2015-09-20 19:25:09 -0600

    curl

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Display filename of the URL</title>
+</head>
+<body>
+<h2>Problem: display filename only of the url.</h2>
+
+<?php
+require_once('mylib.php');
+locateIPS();
+
+?>
+</body>
+</html>
